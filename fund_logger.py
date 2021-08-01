dc45813165342235f631d219ea8d96b9b0c93ecc
albertjia666 <32834700+albertjia666@users.noreply.github.com>
2021-08-01 09:51:34 +0800

    Update fund_logger.py

--- fund_logger.py
+++ fund_logger.py
@@ -1,7 +1,17 @@
 # -*- coding:utf-8 -*-
 import logging
-import os
+import os.path
 import sys
+import colorlog
+
+#  black, red, green, yellow, blue, purple, cyan, white
+log_colors_config = {
+    'DEBUG': 'white',  # cyan white
+    'INFO': 'green',
+    'WARNING': 'yellow',
+    'ERROR': 'purple',
+    'CRITICAL': 'bold_red',
+}
 
 
 class Log(object):
@@ -9,12 +19,16 @@ class Log(object):
     def __init__(self, file_name=None, log_name=None):
         self.logger = logging.getLogger(file_name)
         self.logger.setLevel(level=logging.INFO)
-        self.console_format = logging.Formatter(fmt='[%(asctime)s] [%(filename)s] [line:%(lineno)d] [%(levelname)s] %(message)s')
-        self.file_format = logging.Formatter(fmt='[%(asctime)s] [%(filename)s] [line:%(lineno)d] [%(levelname)s] %(message)s')
+        self.console_format = colorlog.ColoredFormatter(
+            fmt='%(log_color)s[%(asctime)s] %(message)s',
+            log_colors=log_colors_config
+        )
+        self.file_format = logging.Formatter(
+            fmt='[%(asctime)s] [%(filename)s] [%(funcName)s] [line:%(lineno)d] %(message)s'
+        )
         self.log_dir = os.path.join(sys.path[0], 'logs', log_name)
 
         if not self.logger.handlers:
-
             ch = logging.StreamHandler()
             ch.setLevel(level=logging.INFO)
             ch.setFormatter(self.console_format)
